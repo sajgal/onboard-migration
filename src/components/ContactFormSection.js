bfe02b7b56be742191a56ca456cf9ef0bec826d1
Matej Sajgal <roarbb@gmail.com>
2018-10-26 21:07:04 +1100

    Netlify form submit changed

--- src/components/ContactFormSection.js
+++ src/components/ContactFormSection.js
@@ -1,54 +1,121 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { navigateTo } from 'gatsby-link'
 
-const ContactFormSection = props => {
-  return (
-    <div className="content site-width">
-      <div className="form-box">
-        {props.data && (
-          <div>
-            <h2>{props.data.contactSectionTitle}</h2>
-            <span
-              dangerouslySetInnerHTML={{
-                __html: props.data.contactSectionText.childMarkdownRemark.html,
-              }}
-            />
-          </div>
-        )}
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/">
-          <input type="hidden" name="bot-field" />    
-          <label htmlFor="name">
-            <FormattedMessage id="name" />
-          </label>
-          <FormattedMessage id="namePlaceholder">
-            {text => (
-              <input type="text" name="name" id="name" placeholder={text} />
-            )}
-          </FormattedMessage>
-
-          <label htmlFor="email">
-            <FormattedMessage id="email" />
-          </label>
-          <FormattedMessage id="emailPlaceholder">
-            {text => (
-              <input type="text" name="email" id="email" placeholder={text} />
-            )}
-          </FormattedMessage>
-
-          <label htmlFor="message">
-            <FormattedMessage id="message" />
-          </label>
-          <FormattedMessage id="messagePlaceholder">
-            {text => <textarea name="message" id="text" placeholder={text} />}
-          </FormattedMessage>
-
-          <FormattedMessage id="send">
-            {text => <input type="submit" value={text} />}
-          </FormattedMessage>
-        </form>
-      </div>
-    </div>
-  )
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
 }
 
-export default ContactFormSection
+export default class ContactFormSection extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch(`/${this.props.lang}/`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <div className="content site-width">
+        <div className="form-box">
+          {this.props.data && (
+            <div>
+              <h2>{this.props.data.contactSectionTitle}</h2>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: this.props.data.contactSectionText.childMarkdownRemark
+                    .html,
+                }}
+              />
+            </div>
+          )}
+          <form
+            name="contact"
+            method="post"
+            action={`/${this.props.lang}/thanks/`}
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+
+            <p hidden>
+              <label>
+                Don’t fill this out:{' '}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
+
+            <label htmlFor="name">
+              <FormattedMessage id="name" />
+            </label>
+            <FormattedMessage id="namePlaceholder">
+              {text => (
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder={text}
+                  onChange={this.handleChange}
+                />
+              )}
+            </FormattedMessage>
+
+            <label htmlFor="email">
+              <FormattedMessage id="email" />
+            </label>
+            <FormattedMessage id="emailPlaceholder">
+              {text => (
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder={text}
+                  onChange={this.handleChange}
+                />
+              )}
+            </FormattedMessage>
+
+            <label htmlFor="message">
+              <FormattedMessage id="message" />
+            </label>
+            <FormattedMessage id="messagePlaceholder">
+              {text => (
+                <textarea
+                  name="message"
+                  id="text"
+                  placeholder={text}
+                  onChange={this.handleChange}
+                />
+              )}
+            </FormattedMessage>
+
+            <FormattedMessage id="send">
+              {text => <input type="submit" value={text} />}
+            </FormattedMessage>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
